db570a3b2c90ff0b1a727e7be87685479df2ca6b
prajwal-2006 <rprajwal9845@gmail.com>
2025-02-12 18:42:16 +0530

    Add files via upload
    The attached file is a Java Code to convert a String into an Integer using the Integer.parseInt() method and display the Converted Integer.

---
+++ String_to_Integer.java
@@ -0,0 +1,9 @@
+public class String_to_Integer
+{
+public static void main(String args[])
+{
+String text="123";
+int num=Integer.parseInt(text);
+System.out.println("Converted Integer is: "+num);
+}
+}
